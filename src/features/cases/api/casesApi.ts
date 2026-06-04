3470297a0be86c1c155b38ba10064d67688c2d85
hatopoppo404 <simamura404@gmail.com>
2026-06-04 13:54:46 +0900

    通信追加

--- src/features/cases/api/casesApi.ts
+++ src/features/cases/api/casesApi.ts
@@ -2,8 +2,13 @@ import type { Case } from "@/types/case";
 import { saveCases, loadCases } from "../utils/casesStrage";
 
 export const getCases = async (): Promise<Case[] | null> => {
-  return loadCases();
+  const response = await fetch(
+    "/api/cases",
+  );
+
+  return response.json();
 };
+
 export const saveCasesApi = async (cases: Case[]): Promise<void> => {
   await saveCases(cases);
 };
