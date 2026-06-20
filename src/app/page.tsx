6a844299ca2e6f3a8e6377cea9c87d2750661717
tatopoppo404 <simamura404@gmail.com>
2026-06-20 15:33:33 +0900

    localStorage卒業

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
 "use client";
-import type { Case } from "@/types/case";
-import type { SortKey } from "@/features/cases/sort/sortOptions";
-import { sortCases } from "@/features/cases/utils/sortCases";
-import { Card } from "@/features/cases/components/Card";
-import clsx from "clsx";
-import { SearchInput } from "@/features/cases/components/SearchInput";
-import { SortSet } from "@/features/cases/components/SortSet";
-import { useState } from "react";
-import { doesCaseMatchSearch } from "@/features/cases/utils/search";
-import { CaseList } from "@/features/cases/components/CaseList";
-import { mockCases } from "@/features/cases/data/mockCases";
 import { CasesPage } from "@/features/cases/components/CasesPage";
 
 
