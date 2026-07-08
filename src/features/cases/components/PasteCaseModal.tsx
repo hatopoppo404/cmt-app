f7b92fdbdf23933e6f6e1a1d96fa2b164301663b
hatopoppo404 <simamura404@gmail.com>
2026-07-08 10:40:30 +0900

    PasteCaseModal作成

---
+++ src/features/cases/components/PasteCaseModal.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  onClose: () => void;
+};
+
+export const PasteCaseModal = ({ onClose }: Props) => {
+  return (
+    <div>
+      <section>
+        <h1></h1>
+      </section>
+    </div>
+  );
+};
