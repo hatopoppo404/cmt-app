dea0fac7310bdda00577f7ebe15ba81195fa3d27
tatopoppo404 <simamura404@gmail.com>
2026-08-10 16:16:35 +0900

    feat: ウィンドウサイズの設定を追加し、package.jsonに開発用スクリプトを追加

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,22 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, screen } from "electron";
 
 app.whenReady().then(() => {
+  // windowサイズの設定
+  const { width: screenWidth, height: screenHeight } =
+    screen.getPrimaryDisplay().workAreaSize;
+  const windowWidth = 700;
+  const windowHeight = Math.min(1200, screenHeight);
+  const minHeight = Math.min(400, screenHeight);
+  const minWidth = windowWidth;
+
+  // メインウィンドウの定義
   const mainWindow = new BrowserWindow({
-    height: 800,
-    width: 600,
+    width: windowWidth,
+    height: windowHeight,
+    minHeight: minHeight,
+    minWidth: minWidth,
+    title: "cmt-app",
+    titleBarStyle: "hidden",
   });
   mainWindow.loadURL("http://localhost:3000");
 });
